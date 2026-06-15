badeade6f00b751fb9c406fb810ab73ff8d4d47c
Tsipi <tsipi@dreamsdesigners.com>
2026-06-15 11:38:18 +0300

    return alembic files to root folder

--- alembic/env.py
+++ alembic/env.py
@@ -12,8 +12,8 @@ from sqlalchemy.ext.asyncio import create_async_engine
 
 load_dotenv(find_dotenv(usecwd=True), override=True)
 
-from backend.storage.database import Base, _async_url  # noqa: E402
-import backend.storage.db_models  # noqa: E402, F401
+from storage.database import Base, _async_url  # noqa: E402
+import storage.db_models  # noqa: E402, F401
 
 alembic_cfg = context.config
 if alembic_cfg.config_file_name is not None:
